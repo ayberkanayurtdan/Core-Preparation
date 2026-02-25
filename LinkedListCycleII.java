3f0b7cfe42589338ed35891f1006c4a17d4d8edf
Ayberk Anayurtdan <ayberkanayurtdan2@gmail.com>
2026-02-25 21:41:02 +0300

    Solve LinkedList Cycle II with Floyd's Tortoise and Hare (Optimal)

---
+++ LinkedListCycleII.java
@@ -0,0 +1,21 @@
+public class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(fast!=slow){
+                    fast=fast.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
