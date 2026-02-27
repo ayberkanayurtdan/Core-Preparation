8fde35b674530b323ab843c41a3edcc1efd0f7ba
Ayberk Anayurtdan <ayberkanayurtdan2@gmail.com>
2026-02-28 00:26:13 +0300

    Solve Three Sum with 'Sorting and Two Pointers' (Optimal)

---
+++ ThreeSum.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+//Sorting and Two Pointers (Optimal) - Time Complexity= O(n^2) - Space Complexity= O(log n) to O(n)
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> result= new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]>0) break;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1;
+            int r=nums.length-1;
+
+            while(l<r){
+                int sum= nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    result.add(Arrays.asList(nums[i],nums[l],nums[r]));
+
+                    while(l<r && nums[l]==nums[l+1]) l++;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+
+                    l++;
+                    r--;
+                }else if(sum<0){
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+        return result;
+    }
+}
