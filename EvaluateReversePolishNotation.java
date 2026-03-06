b4e9ab91d3e4e110e4a6d7892533288199a56a18
Ayberk Anayurtdan <ayberkanayurtdan2@gmail.com>
2026-03-06 22:37:07 +0300

    Solve Evaluate Reverse Polish Notation with Stack (Optimal)

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    //Stack (Optimal) - Time Complexity: O(n) - Space Complexity: O(n)
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for(String s:tokens){
+            switch(s){
+                case "+":
+                    stack.push(stack.pop()+stack.pop());
+                    break;
+                case "-":{
+                    int num1=stack.pop();
+                    int num2=stack.pop();
+                    stack.push(num2-num1);
+                    break;}
+                case "*":
+                    stack.push(stack.pop()*stack.pop());
+                    break;
+                case "/":{
+                    int num1=stack.pop();
+                    int num2=stack.pop();
+                    stack.push(num2/num1);
+                    break;}
+                default:
+                    stack.push(Integer.parseInt(s));
+                    break;         
+            }
+        }
+        return stack.pop();
+    }
+}
