7c7a6f8bd064884bd8383cc5769154df70767616
Ayberk Anayurtdan <ayberkanayurtdan2@gmail.com>
2026-03-13 20:41:37 +0300

    Solve Kth Smallest Element in a BST with Inorder Traversal (Optimal)

---
+++ KthSmallestElementInABST.java
@@ -0,0 +1,29 @@
+public class KthSmallestElementInABST {
+    // Inorder Traversal Approach (Optimal) - Time Complexity: O(n) - Space Complexity: O(h)
+    int count=0;
+    public int kthSmallest(TreeNode root, int k) {
+        if(root==null) return -1;
+
+        int left=kthSmallest(root.left,k);
+        if(left!=-1) return left;
+
+        count++;
+        if(count==k) return root.val;
+
+        return kthSmallest(root.right, k);
+    }
+
+    @SuppressWarnings("unused")
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
